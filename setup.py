4f3ef3b8932528092d79e0ff2d1d69d70e2f139c
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-08 16:00:30 +0100

    Move sources to usual subfolder

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "hotdoc-gi-extension",
+    name = "hotdoc_gi_extension",
     version = "0.6",
-    keywords = "C clang hotdoc",
-    url='https://github.com/MathieuDuponchelle/hotdoc-c-extension',
+    keywords = "gobject-introspection C hotdoc",
+    url='https://github.com/hotdoc/hotdoc_gi_extension',
     author_email = 'mathieu.duponchelle@opencreed.com',
     license = 'LGPL',
     description = "An extension for hotdoc that parses gir files",
     author = "Mathieu Duponchelle",
     packages = find_packages(),
-    entry_points = {'hotdoc.extensions': 'get_extension_classes = gi_extension:get_extension_classes'},
+
+    package_data = {
+        '': ['*.html'],
+    },
+
+    entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_gi_extension.gi_extension:get_extension_classes'},
     install_requires = [
         'hotdoc>=0.6',
         'lxml',
-    ]
+    ],
 )
