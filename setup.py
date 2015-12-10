80433b1fe707f5deb1c8fbb88baeee49a4b05162
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-10 18:13:05 +0100

    0.6.3 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "hotdoc_gi_extension",
-    version = "0.6.2",
+    version = "0.6.3",
     keywords = "gobject-introspection C hotdoc",
     url='https://github.com/hotdoc/hotdoc_gi_extension',
     author_email = 'mathieu.duponchelle@opencreed.com',
@@ -18,8 +18,8 @@ setup(
 
     entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_gi_extension.gi_extension:get_extension_classes'},
     install_requires = [
-        'hotdoc>=0.6.2',
-        'hotdoc_c_extension>=0.6.2',
+        'hotdoc>=0.6.3',
+        'hotdoc_c_extension>=0.6.3',
         'lxml',
     ],
 )
