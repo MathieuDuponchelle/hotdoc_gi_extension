323d685f8cbb33609b92c0bee493d3420b24b901
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-03-07 13:21:58 +0100

    License hotdoc's GI extension to LGPLv2.1+

--- hotdoc_gi_extension/fundamentals.py
+++ hotdoc_gi_extension/fundamentals.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2015,2016 Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
+# Copyright © 2015,2016 Collabora Ltd
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 from hotdoc.core.links import Link
 
 
@@ -109,7 +127,7 @@ def _create_python_fundamentals():
             "gboolean": boolean_link,
             "gint8": integer_link,
             "guint8": integer_link,
-            "gint16": integer_link,
+            "gint15": integer_link,
             "guint16": integer_link,
             "gint32": integer_link,
             "guint32": integer_link,
